d27cc7b3c2eb7b85f7cf52ca7d2aac61a4a63df0
brokoliboro <brokoli@marketboro.com>
2020-11-02 11:13:05 +0900

    Initial Commit

---
+++ sandbox/sandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandboxApp.swift
+//  sandbox
+//
+//  Created by Hyunseung Choi on 2020/11/02.
+//
+
+import SwiftUI
+
+@main
+struct sandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
